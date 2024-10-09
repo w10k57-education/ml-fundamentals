c6dbb140a3ac56fd32330813f86ac899026069a8
Marek <mysior.marek@gmail.com>
2024-10-09 14:11:55 +0200

    Create functions.py file in 'src'

--- src/functions.py
+++ src/functions.py
@@ -12,27 +12,6 @@ The module provides tools for creating test datasets, evaluating linear models,
 and implementing gradient descent optimization for linear regression tasks.
 """
 
-import numpy as np
-
-def generate_data(m_samples, x_max=1, a_range=10,
-                  b_range=1, noise=0.2, coeff=False):
-    """
-    Generate synthetic data for linear regression.
-
-    Returns x and y arrays for linear regression analysis.
-    """
-    rg = np.random.default_rng(seed=18)
-    a_data = rg.uniform(-a_range, a_range)
-    b_data = rg.uniform(-b_range, b_range)
-    x_features = rg.uniform(0, x_max, m_samples)
-    noise_stddev = rg.uniform(noise/2, noise)
-    y_features = (a_data * x_features + b_data +
-                  np.random.normal(0, noise_stddev, m_samples))
-    if coeff is True:
-        print(f'Coefficients used to generate data: \n'
-              f'a = {a_data:.2f} \nb = {b_data:.2f}')
-    return x_features, y_features
-
 def linear_function(x, a, b):
     """
     Calculate linear function y = ax + b.
